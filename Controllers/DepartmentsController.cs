8e91ee05f871a19751d3ab239aa4f24436fafa26
Vitor Costa <csvitor.dev@gmail.com>
2024-05-17 16:28:45 -0300

    feat: Department Model-View-Controller created

---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private List<Department> _departments = new();
+        public IActionResult Index()
+        {
+            _departments.Add(new(1, "Eletronics"));
+            _departments.Add(new(2, "Fashion"));
+
+            return View(_departments);
+        }
+    }
+}
---
+++ Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Department(int id, string name)
+    {
+        public int Id { get; } = id;
+        public string Name { get; set; } = name;
+    }
+}
