1f8122d77e864c80dd8c1fc4292004b198f85cec
csvitor-dev <csvitor.dev@gmail.com>
2024-11-08 11:13:09 -0300

    build: changing connection string to support Docker container

--- Configuration.cs
+++ Configuration.cs
@@ -2,7 +2,7 @@ namespace SalesWebMVC
 {
     public static class Configuration
     {
-        public const string ConnectionString = "server=localhost;database=saleswebmvcdb;user=MVCuser;password=mVC#1246;";
+        public const string ConnectionString = "server=host.docker.internal;port=3306;database=salesdb;user=user1;password=mVC#1246";
         public const string GitHubDefaultUsername = "csvitor-dev";
     }
 }
