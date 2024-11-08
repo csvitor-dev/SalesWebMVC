46c0c70ca75def93f0f27fbf8a9a842d5a7029bd
csvitor-dev <csvitor.dev@gmail.com>
2024-11-08 11:14:56 -0300

    refactor: 'set' property -> 'init'

--- Data/SalesWebMvcContext.cs
+++ Data/SalesWebMvcContext.cs
@@ -2,9 +2,9 @@
 {
     public class SalesWebMvcContext(DbContextOptions<SalesWebMvcContext> options) : DbContext(options)
     {
-        public DbSet<Department> Department { get; set; } = default!;
-        public DbSet<Seller> Seller { get; set; } = default!;
-        public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
+        public DbSet<Department> Department { get; init; } = default!;
+        public DbSet<Seller> Seller { get; init; } = default!;
+        public DbSet<SalesRecord> SalesRecord { get; init; } = default!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
